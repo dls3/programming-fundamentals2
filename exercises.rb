f7977e5ab8370eb1cc0fb20da78b6e57697e0254
Doug <doug.steinberg@gmail.com>
2017-05-16 13:30:19 -0400

    Exercise 3 complete

--- exercises.rb
+++ exercises.rb
@@ -1,17 +1,17 @@
 #### Exercise 0 ####
 fav_colours = ["blue", "gray", "black", "purple", "white"]
-ages = [14, 26, 31, 34, 36]
+ages = [36, 31, 26, 34, 14]
 coin_heads = [true, false, false, true, false]
 performing_artists = ["eminem", "the killers", "oasis"]
-fav_colours_sym = [:blue, :gray, :black, :purple, :white]
+fav_colours_sym = [:'blue', :'gray', :'black', :'purple', :'white']
 
 
 words = {big: "of considerable size, extent, or intensity.",
   small: "of a size that is less than normal or usual.",
   medium: "about halfway between two extremes"}
-movies = {anchorman: 2004, gladiator: 2000, goodwillhunting: 1997}
-cities = {vancouver: 2135201, montreal: 4098927, toronto: 5132794}
-siblings = {michael: 14, rebecca: 31, david: 34, ben: 35}
+movies = {'anchorman': 2004, 'gladiator': 2000, 'good will hunting': 1997}
+cities = {'vancouver': 2135201, 'montreal': 4098927, 'toronto': 5132794}
+siblings = {'michael': 14, 'rebecca': 31, 'david': 34, 'ben': 35}
 
 
 #### Exercise 1 ####
@@ -19,25 +19,30 @@ puts coin_heads
 puts fav_colours[0]
 puts ages.sort
 ages << 0
-puts movies[:anchorman]
+puts movies[:'anchorman']
 
 #### Exercise 2 ####
 puts fav_colours[fav_colours.length-1]
-cities[:london] = 9787426
+cities[:'london'] = 9787426
 puts coin_heads.reverse
-puts cities[:vancouver]
+puts cities[:'vancouver']
 
 performing_artists.each do |artist|
   puts "I think #{artist} is a phenomenal performer"
 end
 
-
-
-
-
 #### Exercise 3 ####
+puts performing_artists[0],performing_artists[1]
 
+movies.each do |movie, year|
+  puts "#{movie} came out in #{year}."
+end
 
+puts ages.sort.reverse
+
+movies[:'beaty and the beast'] = 1991, 2017
+#check that it worked:
+puts movies
 
 #### Exercise 4 ####
 

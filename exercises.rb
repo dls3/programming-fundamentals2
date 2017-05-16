fa4224e0e96764009f08c835b79574eb70c82fd9
Doug <doug.steinberg@gmail.com>
2017-05-16 15:47:04 -0400

    Exercise 6 complete

--- exercises.rb
+++ exercises.rb
@@ -9,20 +9,25 @@ fav_colours_sym = [:blue, :gray, :black, :purple, :white]
 words = {big: "of considerable size, extent, or intensity.",
   small: "of a size that is less than normal or usual.",
   medium: "about halfway between two extremes"}
-movies = {'anchorman': 2004, 'gladiator': 2000, 'good will hunting': 1997}
+movies = {'anchorman': 2004, gladiator: 2000, 'good will hunting': 1997}
 cities = {'vancouver': 2135201, 'montreal': 4098927, 'toronto': 5132794}
 siblings = {'michael': 14, 'rebecca': 31, 'david': 34, 'ben': 35}
-
+#
+# key: value
+# 'key' => value
+# :key => value
+# 'key': value
+# :'good will hunting' => value
 
 #### Exercise 1 ####
 puts coin_heads
-puts fav_colours_sym[0]
+puts fav_colours[0]
 puts ages.sort
 ages << 0
 puts movies[:'anchorman']
 
 #### Exercise 2 ####
-puts fav_colours_sym[fav_colours_sym.length-1]
+puts fav_colours[fav_colours.length-1]
 cities[:'london'] = 9787426
 puts coin_heads.reverse
 puts cities[:'vancouver']
@@ -92,12 +97,82 @@ ages.each do |age|
   age += 1
   puts "Age plus one is #{age}"
 end
+puts ages
 
+# Using map
+# new_ages = ages.map do |age|
+#   age += 1
+# end
+# puts new_ages
 
 # Add two new colours to array of favourite colours.
 fav_colours_sym.push(:crimson, :salmon)
 puts fav_colours_sym
 
 
-
 #### Exercise 6 ####
+#
+# Make a new hash that contains a list of movies for each year. Each list of movies should be an array.
+movies_year = {
+  '1999' => ["the matrix", "fight club", "american beauty"],
+  '2004' => ["the incredibles", "mean girls", "collateral"],
+  '2009' => ["avatar", "watchmen", "coraline"]
+}
+
+# Make a new array that contains each row of the buttons on a phone. Each row should be an array.
+phone_buttons = [['1','2','3'],['4','5','6'],['7','8','9'],['*','0','#']]
+
+# Make a new array that contains information about three countries.
+#Each country should be a hash that has a name, a continent, and whether or not it is an island.
+countries = [
+  {name: 'canada', continent: 'north america', island: false},
+  {name: 'UK', continent: 'europe', island: true},
+  {name: 'south africa', continent: 'africa', island: false}
+]
+
+# Output the message "I will not skateboard in the halls" 20 times.
+20.times do
+  puts "I will not skateboard in the halls"
+end
+
+# Create an array consisting of the above message. It should appear in the array 20 times.
+text = []
+20.times do
+  text.push("I will not skateboard in the halls")
+end
+
+# Create an array consisting of the numbers between 1 and 50.
+nums = []
+50.times do |num|
+  nums.push(num+1)
+end
+
+# Use an each loop to find the sum of the numbers in the above array.
+sum = 0
+(1..50).each do |row|
+  # puts nums[num]
+  sum = sum + nums[row-1]
+end
+puts sum
+
+# Create a new array which has three of each number up to 50.
+# Ie. [1, 1, 1, 2, 2, 2, 3, 3, 3, ... , 50, 50, 50] and so on, up to 50.
+one_to_fifty = []
+50.times do |num|
+  3.times do
+    one_to_fifty.push(num+1)
+  end
+end
+puts one_to_fifty
+
+# Make a new array out all of the countries from the hash in Exercise 6 that are not islands.
+#Print out both arrays.
+
+not_islands = []
+
+countries.each do |value|
+  if value[:island] == false
+    not_islands.push(value[:name])
+  end
+end
+puts not_islands

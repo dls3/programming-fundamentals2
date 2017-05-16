ae48b79286f8e78403a423a51791404970a05155
Doug <doug.steinberg@gmail.com>
2017-05-16 12:57:06 -0400

    Exercise 0 complete

---
+++ exercises.rb
@@ -0,0 +1,15 @@
+#### Exercise 0 ####
+
+fav_colours = ["blue", "gray", "black", "purple", "white"]
+ages = [14, 26, 31, 34, 36]
+coin_heads = [true, false, false, true, false]
+performing_artists = ["eminem", "the killers", "oasis"]
+fav_colours_sym = [:blue, :gray, :black, :purple, :white]
+
+
+words = {big: "of considerable size, extent, or intensity.",
+  small: "of a size that is less than normal or usual.",
+  medium: "about halfway between two extremes"}
+movies = {anchorman: 2004, gladiator: 2000, goodwillhunting: 1997}
+cities = {vancouver: 2135201, montreal: 4098927, toronto: 5132794}
+siblings = {michael: 14, rebecca: 31, david: 34, ben: 35}
